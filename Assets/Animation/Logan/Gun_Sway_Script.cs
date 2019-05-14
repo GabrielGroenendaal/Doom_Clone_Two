6d1ad2d9962643b46d23a7a89ee1a61656d09fe0
salty68 <lck295@nyu.edu>
2019-05-14 03:02:27 -0400

    Head Bobbing on player

---
+++ Assets/Animation/Logan/Gun_Sway_Script.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun_Sway_Script : MonoBehaviour
+{
+    public Animator gun_sway;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gun_sway.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            gun_sway.speed += 0.2f;
+            if (gun_sway.speed > 1f)
+            {
+                gun_sway.speed = 1f;
+            }
+        }
+        else
+        {
+            gun_sway.speed = 0f;
+        }
+    }
+}
